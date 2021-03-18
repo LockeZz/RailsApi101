af1fdf8c46faee5f14a562a3919dc5263dc42110
p2digital <LockeZz.PHF@gmail.com>
2021-03-18 09:17:24 +0800

    Error Handling and jwt

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,15 +2,32 @@ module ExceptionHandler
 
     extend ActiveSupport::Concern 
 
+    # Define custom error subclasses - rescue catches 'Standard Errors'
+    class AuthenticationErrror < StandardError; end 
+    class MissingToken < StandardError; end 
+    class Invalidtoken < StandardError; end
+
     included do 
+        #Define custom handlers
+        rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+        rescue_from ExceptionHandler::AuthenticationErrror, with: :unauthorized_request
+        rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+        rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
+
         rescue_from ActiveRecord::RecordNotFound do |e| 
             json_response({ message: e.message }, :not_found)
         end
+    end
 
-        rescue_from ActiveRecord::RecordInvalid do |e| 
+    private 
+        #JSON response with message; Status code 422- unprocessable entity
+
+        def four_twenty_two(e)
             json_response({ message: e.message }, :unprocessable_entity)
         end
 
-    end
-
+        def unauthorized_request(e)
+            json_response({ message: e.message }, :unauthorized)
+        end
 end
